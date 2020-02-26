db638c51d86db48a36f1c03f3e3cc8917dcad247
Phill Peters <phillpeters1@gmail.com>
2020-02-25 21:13:18 -0700

    finalize project

--- script.js
+++ script.js
@@ -10,11 +10,32 @@ function formatQueryParams(params) {
   return queryItems.join('&');
 }
 
+function displayResults(responseJson) {
+  $('.results-list, .js-error-message').empty();
+
+  for (let i = 0; i < responseJson.total; i++) {
+    $('.results-list').append(
+     `<li>
+        <h3>${responseJson.data[i].fullName}</h3>
+        <p>${responseJson.data[i].description}</p>
+        <address>
+          ${responseJson.data[i].addresses[1].line1}<br>
+          ${responseJson.data[i].addresses[1].city}, ${responseJson.data[i].addresses[1].stateCode}<br>
+          ${responseJson.data[i].addresses[1].postalCode}
+        </address>
+        <a href="${responseJson.data[i].url}"}>${responseJson.data[i].url}</a>
+      </li>`
+    );
+  }
+
+}
+
 function getParksList(searchTerm, state, maxResults) {
   const params = {
     q: searchTerm,
     stateCode: state,
     limit: maxResults,
+    fields: 'addresses',
     api_key: apiKey
   };
   const queryString = formatQueryParams(params);
@@ -29,7 +50,10 @@ function getParksList(searchTerm, state, maxResults) {
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => console.log(responseJson))
+    .then(responseJson => {
+      console.log(responseJson);
+      displayResults(responseJson);
+    })
     .catch(err => {
       $('.js-error-message').text(`Something went wrong: ${err.message}`)
     });
